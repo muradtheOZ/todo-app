161ca39dfed013528a994e132c44d9c2e188d444
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-05-22 23:07:45 +0600

    huge Update succeded

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, Alert, View } from 'react-native';
+import { FlatList, StyleSheet, Alert, View,Text } from 'react-native';
 import AddTodos from './Components/AddTodos/AddTodos';
 import Header from './Components/Header/Header';
 import Todos from './Components/Todos/Todos';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AppLoading from "expo-app-loading";
+import Completed from './Components/Completed/Completed';
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const initialTodos = []
   const [todos, setTodos] = useState(initialTodos);
+  const [completed, setCompleted] = useState(null);
+
+  const deleteHandler = (key) =>{
+    const newComplete = completed.filter(todo => todo.key != key)
+    const completeJasonValue = JSON.stringify(newComplete)
+    AsyncStorage.setItem('completedToDos', completeJasonValue)
+    .then(()=>{
+      setCompleted(newComplete);
+    })
+  }
 
   const doneHandler = (key) => {
     Alert.alert(
@@ -29,9 +40,23 @@ export default function App() {
           }
         },
         {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
+          text: "Completed",
+          onPress: () => {
+            const newComplete = todos.filter(todo => todo.key == key)
+            const ToatlCompleted = [...newComplete,...completed]
+            const completeJasonValue = JSON.stringify(ToatlCompleted)
+            AsyncStorage.setItem('completedToDos', completeJasonValue)
+            .then(()=>{
+              setCompleted(ToatlCompleted);
+            })
+            
+            const newTodos = todos.filter(todo=>todo.key != key)
+            const jsonValue = JSON.stringify(newTodos)
+            AsyncStorage.setItem('storedToDos', jsonValue)
+            .then(()=>{
+              setTodos(newTodos);
+            })
+          }
         },
         { text: "OK", onPress: () => console.log("OK Pressed") }
       ]
@@ -79,6 +104,16 @@ export default function App() {
       .catch(error => {
         console.log(error)
       })
+
+      AsyncStorage.getItem('completedToDos')
+      .then(data => {
+        if (data !== null) {
+          setCompleted(JSON.parse(data))
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   if (!ready) {
@@ -109,6 +144,17 @@ export default function App() {
 
         </View>
 
+        <View style={styles.completeList}>
+          <Text style={styles.title}> Completed List</Text>
+          <FlatList
+            data={completed}
+            renderItem={({ item }) => (
+              <Completed item={item} setTodos={setTodos} deleteHandler={deleteHandler} />
+            )}
+          />
+
+        </View>
+
       </View>
     </View>
   );
@@ -130,4 +176,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  completeList:{
+    flex: 1,
+    marginTop: 20,
+  },
+  title:{
+    textAlign:'center',
+    backgroundColor:'#3450A1',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    color: 'white',
+    fontSize: 20
+  }
 });
---
+++ Components/Completed/Completed.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View,StyleSheet,TouchableOpacity } from 'react-native';
+
+const Completed = ({item,deleteHandler}) => (
+    <TouchableOpacity onPress={()=>deleteHandler(item.key) }>
+    <Text style = {styles.list}>{item.text}</Text>
+</TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+    list: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: '#bbb',
+        borderWidth: 1,
+        borderStyle: "dashed",
+        borderRadius: 10
+      }
+   
+  });
+  
+
+export default Completed;
